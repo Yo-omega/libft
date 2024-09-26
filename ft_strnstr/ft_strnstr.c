51506a0aea53bb7748f2d0462fe95d81938a5ada
Yo-omega <elbouzidyyousef@gmail.com>
2024-09-26 18:40:55 +0100

    strnstr

--- ft_strnstr/ft_strnstr.c
+++ ft_strnstr/ft_strnstr.c
@@ -6,25 +6,24 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 12:46:06 by kali              #+#    #+#             */
-/*   Updated: 2024/09/26 18:37:49 by kali             ###   ########.fr       */
+/*   Updated: 2024/09/26 18:39:52 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strnstr(char *big, const char *little, int len)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	j = 0;
 	while (big[j] && j < len)
 	{
 		i = 0;
 		while (little[i] && little[i] == big[j + i])
-			i++; 
+			i++;
 		if (little[i] == 0)
-			return &big[j];
+			return (&big[j]);
 		j++;
 	}
 	return (&big[j]);
 }
-
