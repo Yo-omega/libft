ee5cd230634154a806ef17b25467b0125c3fc4db
Yo-omega <elbouzidyyousef@gmail.com>
2024-09-27 19:42:57 +0100

    calloc based on bzero and memset

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 17:34:00 by kali              #+#    #+#             */
+/*   Updated: 2024/09/27 17:55:29 by kali             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void *ft_memset(void *s, int c, size_t n);
+void ft_bzero(void *s, size_t n);
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	void	*p;
+
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	ft_bzero(p, size * nmemb);
+	return (p);
+}
