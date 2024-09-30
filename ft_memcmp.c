9256ae2d086030a9ce53fcdebf855fc6ec008f4c
Yo-omega <elbouzidyyousef@gmail.com>
2024-09-30 21:25:27 +0100

    memcmp using strncmp

---
+++ ft_memcmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 20:20:13 by kali              #+#    #+#             */
+/*   Updated: 2024/09/30 21:05:58 by kali             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*str1;
+	char	*str2;
+
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	return (ft_strncmp(str1, str2, n));
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && i < n - 1)
+		i++;
+	return (s1[i] - s2[i]);
+}
