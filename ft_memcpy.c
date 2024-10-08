4bbb09a9a45f6e85d6188509b2bd9b59caeffe8d
Yo-omega <elbouzidyyousef@gmail.com>
2024-10-08 19:50:06 +0100

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -2,5 +2,20 @@
 
 void *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    
+    char    *sour;
+    char    *dst;
+    int     i;
+
+    sour = (char *)src;
+    dst = (char *)dest;
+    i = 0;
+    while (sour[i])
+    {
+        dst[i] = sour[i];
+        i++;
+    }
+    src = (void *)sour;
+    dest = (void *)dst;
+    return (dest);
+   
 }
